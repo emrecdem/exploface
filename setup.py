3a400097ea96080b5399023964bc04c4591586f5
bldevries-escience <b.devries@esciencecenter.nl>
2018-07-18 15:30:20 +0200

    Moved to dev3 version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setuptools.setup(name='exploface',
-      version='0.0.0.dev2',
+      version='0.0.0.dev3',
       description='',
       long_description=readme(),
       long_description_content_type='text/markdown',
